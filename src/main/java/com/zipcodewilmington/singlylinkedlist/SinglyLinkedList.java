f43cfaf084fcb7ee95be33556479972d1608568e
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-03-30 22:15:40 -0400

    Done add method

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,50 @@
-package com.zipcodewilmington.singlylinkedlist;
+    package com.zipcodewilmington.singlylinkedlist;
+    /**
+         * Created by leon on 1/10/18.
+         */
+        public class SinglyLinkedList {
+        private Node head;
+        private int size;
+        private int data;
+        private class Node {
+            int data;
+            Node next;
+            Node(int data){
+                this.data=data;
+                this.next=null;
+            }
+        }
+        public SinglyLinkedList(){
+            head=null;
+            size=0;
+        }
+        public void add(int data){
+            Node node=new Node(data);
+            node.data=data;
+            if(node==null){
+                node=head;
+            }else{
+                Node n=head;
+               while(n.next!=null){
+                    n=n.next;
+                }
+               n.next=node;
+            }
 
-/**
- * Created by leon on 1/10/18.
- */
-public class SinglyLinkedList {
-}
+        public void remove(int index){
+            for(int i=0;i<index-1;i++){
+
+            }
+
+        }
+
+
+
+
+
+    }}
+
+
+
+
+
